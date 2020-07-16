4f5f1b8c9e215b73447e54d6ede7017be469b4c3
Vipin GS <vipings@aol.in>
2020-07-16 14:31:11 +0530

    Added Sign In , Sign Out functionalities.Changed applicaton name and logo

--- src/App.js
+++ src/App.js
@@ -3,39 +3,74 @@ import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import Rank from './components/rank/rank';
 import ImgaeLinkForm from './components/imgaeLinkForm/imgaeLinkForm';
+import SignIn from './components/signIn/signIn';
 import Particles from 'react-particles-js';
+import Register from './components/register/register';
 import './App.css';
 
-const paramproperties ={
+
+const paramproperties = {
   "particles": {
-      "number": {
-          "value": 100
-      },
-      "size": {
-          "value": 5
-      }
+    "number": {
+      "value": 100
+    },
+    "size": {
+      "value": 5
+    }
   },
   "interactivity": {
-      "events": {
-          "onhover": {
-              "enable": true,
-              "mode": "repulse"
-          }
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
       }
+    }
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particleproperties" 
-       params={paramproperties} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImgaeLinkForm />
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      route: 'signin',
+      isSignedIn : false
+    }
+  }
+
+  onRouteChange = (route) => {
+    if( route === 'home')
+    {
+      this.setState({ isSignedIn : true})
+    }
+    else
+    {
+      this.setState({ isSignedIn : false})
+    }
+    this.setState({ route: route })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particleproperties"
+          params={paramproperties} />
+        <Navigation onRouteChange={this.onRouteChange} isSignedIn={this.state.isSignedIn}/>
+        {this.state.route === 'home' ?
+          <div>
+            <Logo />
+            <Rank />
+            <ImgaeLinkForm />
+          </div>
+          :
+          (this.state.route === 'signin' ?
+            <SignIn onRouteChange={this.onRouteChange} />
+            : <Register onRouteChange={this.onRouteChange}/>
+          )
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-const Navigation = () => {
-    return (
-        <nav style={{ display : 'flex' , justifyContent : 'flex-end'}}>
-            <p className='f4 black dim link pa3 pointer underline'>Sign Out</p>
-        </nav>
-    )
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+
+    if (isSignedIn) {
+        return (
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <p
+                    onClick={() => onRouteChange('signin')}
+                    className='f4 black dim link pa3 pointer underline'>Sign Out</p>
+            </nav>
+        )
+    }
+    else {
+        return (
+            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <p
+                    onClick={() => onRouteChange('signin')}
+                    className='f4 black dim link pa3 pointer underline'>Sign In</p>
+                <p
+                    onClick={() => onRouteChange('register')}
+                    className='f4 black dim link pa3 pointer underline'>Register</p>
+            </nav>
+        )
+    }
+
 }
 
 export default Navigation;
