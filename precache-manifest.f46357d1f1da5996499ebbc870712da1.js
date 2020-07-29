8d9af0f511ecabf0d0276c4da3297501635657af
Vipin GS <vipings@aol.in>
2020-07-29 12:09:32 +0530

    Updates

---
+++ precache-manifest.f46357d1f1da5996499ebbc870712da1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345212014d08317d114adac2cfdd0c7a",
+    "url": "/FaceRecognitionApp/index.html"
+  },
+  {
+    "revision": "c9e606b1039c9bb53589",
+    "url": "/FaceRecognitionApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "77ad8bd1f5bb5652ca54",
+    "url": "/FaceRecognitionApp/static/css/main.b4e23312.chunk.css"
+  },
+  {
+    "revision": "c9e606b1039c9bb53589",
+    "url": "/FaceRecognitionApp/static/js/2.2010fa1b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceRecognitionApp/static/js/2.2010fa1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ad8bd1f5bb5652ca54",
+    "url": "/FaceRecognitionApp/static/js/main.eeaf5f1f.chunk.js"
+  },
+  {
+    "revision": "53ca97dfdc46d2e69ce6",
+    "url": "/FaceRecognitionApp/static/js/runtime-main.001acce0.js"
+  }
+]);
